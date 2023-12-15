bbc8c61a54909f7471b4e00543796569e79bc6e6
kasireddy laxmi prasanna <145850752+laxmiprasanna3@users.noreply.github.com>
2023-12-15 18:33:33 +0000

    commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # todoserverwithnode.js
+
+
+npm i nodemon to install node_modules and package-lock.json
+
+
+Http Methods:
+->GET :getting data from server    //default method
+->POST :sending data to server
+->DELETE :deleting data from server
+->PATCH :updating certain fields
+->PUT :full update
+
+
+
+
+
+
+only GET method is available on the browser
+to get the another methods we should install the "Thunder Client"
+
+
+in thunderclient:
+http://localhost:8081/todos
---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const port = 8081;
+const todolist =["Hey","Hello","how","are","you!!"] 
+http.createServer((req,res)=>{
+const {method, url} = req;
+
+if(url==="/todos"){
+    if(method==="GET"){
+    console.log("todos route, and it's a get method")
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write(todolist.toString())
+}else if(method==="POST"){
+    let body="";
+    req.on('error',(err)=>{
+        console.log(err)
+    }).on('data',(chunk)=>{
+        body += chunk;
+        console.log("chunk:",chunk);
+
+    }).on('end',()=>{
+        body=JSON.parse(body);
+        console.log("data: ",body);
+        let newtodo = todolist;
+        newtodo.push(body.item);
+    })
+}else if(method==="PUT"){
+
+}else if(method==="DELETE"){
+
+}
+else{
+    res.writeHead(404);
+}
+}else if(url==="/"){
+    console.log("/home default route")
+}
+console.log(method,url);
+res.end();
+})
+.listen(port,()=>{
+    console.log(`NodeJs server started running on port ${port}`);
+})
